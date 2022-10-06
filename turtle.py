a099e9b2dc9fafddfd5a27696776f6626dde70ef
yeojeong2201195 <112803862+yeojeong2201195@users.noreply.github.com>
2022-10-06 13:51:02 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,13 @@
+print(7*24)
+
+import turtle
+turtle.shape('turtle')
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
